20b3999b86da4e62fa238e2f691387955211c414
Valso <warut92@gmail.com>
2022-06-16 22:56:55 +0700

    update 4 bars page

--- script.js
+++
@@ -1,9 +0,0 @@
-// https://erikmartinjordan.com/get-random-emoji-javascript
-function getRandomEmoji() {
-  const emoji = ['🍊','🍌','-']
-  const index1 = Math.floor(Math.random() * 3);
-    document.getElementById("bar1").innerHTML = emoji[index1];  
-  const index2 = Math.floor(Math.random() * 2);
-    document.getElementById("bar2").innerHTML = emoji[index2];  
-   
-}
